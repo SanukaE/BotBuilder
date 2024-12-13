fe800a5e486182aed36c1d86c7f112baf9710133
ItzSanuka <ItzSanuka@outlook.com>
2024-12-13 10:46:31 +0530

    [+] Print the text "BotBuilder" on console [+] Add SIGINT & SIGTERM listeners

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Client, GatewayIntentBits } from 'discord.js';
 import 'dotenv/config';
 import eventHandler from './handlers/eventHandler.js';
 import checkEnvVariables from './utils/checkEnvVariables.js';
+import initializeAI from './utils/initializeAI.js';
 
 checkEnvVariables();
 
@@ -16,6 +17,26 @@ const client = new Client({
 });
 
 eventHandler(client);
-//TODO: Delete the log file from file manager
 
 client.login(process.env.APP_TOKEN);
+
+const handleShutdown = async () => {
+  console.log('Shutting down...');
+  client.destroy();
+
+  const { fileManager } = initializeAI();
+
+  if (fileManager) {
+    const uploadedFiles = await fileManager.listFiles();
+
+    for (const file of uploadedFiles.files) {
+      await fileManager.deleteFile(file.name);
+    }
+  }
+
+  console.log('Have a good day! 👋');
+  process.exit(0);
+};
+
+process.on('SIGINT', handleShutdown);
+process.on('SIGTERM', handleShutdown);
