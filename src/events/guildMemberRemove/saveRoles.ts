82ca5340e11316d93143b8a052fc47464024a9c8
ItzSanuka <ItzSanuka@outlook.com>
2025-01-27 17:21:11 +0530

    Implement sticky role management and user roles persistence in MySQL

---
+++ src/events/guildMemberRemove/saveRoles.ts
@@ -0,0 +1,20 @@
+import { Client, GuildMember, PartialGuildMember } from 'discord.js';
+import config from '#config' assert { type: 'json' };
+import MySQL from '#libs/MySQL.js';
+
+export default async function (
+  _: Client,
+  member: GuildMember | PartialGuildMember
+) {
+  const { enableStickyRoles, memberRoleID, botRoleID } = config;
+  if (!enableStickyRoles) return;
+
+  const roleIDs = member.roles.cache
+    .filter((role) => role.id !== memberRoleID && role.id !== botRoleID)
+    .map((role) => role.id);
+
+  await MySQL.query(
+    'INSERT INTO user_roles (userID, roles) VALUES (?, ?) ON DUPLICATE KEY UPDATE roles = ?',
+    [member.id, JSON.stringify(roleIDs), JSON.stringify(roleIDs)]
+  );
+}
