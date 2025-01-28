bd39eb2c1d2ef0c038a8398005276164707bb1c6
ItzSanuka <ItzSanuka@outlook.com>
2025-01-28 15:33:33 +0530

    Add counting feature with message validation and reset functionality

---
+++ src/events/messageCreate/handleCounting.ts
@@ -0,0 +1,78 @@
+import { Client, Colors, Message, OmitPartialGroupDMChannel } from 'discord.js';
+import config from '#config' assert { type: 'json' };
+import createEmbed from '#utils/createEmbed.js';
+
+export default async function (
+  client: Client,
+  message: OmitPartialGroupDMChannel<Message<boolean>>
+) {
+  if (!message.inGuild()) return;
+
+  const { countChannelID, countMuteDuration } = config;
+  if (message.channelId !== countChannelID) return;
+  if (message.author.id === client.user?.id) return;
+
+  const newNumber = Number(message.content);
+
+  if (isNaN(newNumber)) {
+    await message.delete();
+    return;
+  }
+
+  const lastMessage = (
+    await message.channel.messages.fetch({ limit: 1, before: message.id })
+  ).first();
+  const previousNumber = lastMessage ? Number(lastMessage?.content) : 0;
+
+  if (lastMessage && lastMessage.author.id === message.member?.id) {
+    await message.delete();
+    return;
+  }
+
+  if (newNumber !== previousNumber + 1) {
+    const notificationEmbed = createEmbed({
+      color: Colors.Red,
+      title: 'Count destroyed!',
+      description: `The count was destroyed by user \`${message.author.username}\`. The count will now have to start again from \`0\`. I'll start.`,
+      fields: [
+        {
+          name: 'Previous Number',
+          value: `\`${previousNumber}\``,
+          inline: true,
+        },
+        { name: 'Number Entered', value: `\`${newNumber}\``, inline: true },
+      ],
+      thumbnail: {
+        url: 'https://th.bing.com/th/id/OIP.TJ7-kIoyGTTvlkGpNw6MzQHaFg?rs=1&pid=ImgDetMain',
+      },
+    });
+
+    await message.react('❌');
+
+    if (countMuteDuration) {
+      await message.channel.edit({
+        permissionOverwrites: [{ id: message.member!, deny: ['SendMessages'] }],
+      });
+
+      setTimeout(async () => {
+        await message.channel.edit({
+          permissionOverwrites: [
+            { id: message.member!, allow: ['SendMessages'] },
+          ],
+        });
+      }, countMuteDuration * 24 * 60 * 60_000);
+    }
+
+    await message.channel.sendTyping();
+
+    await message.channel.send({
+      embeds: [notificationEmbed],
+    });
+
+    const resetCountMsg = await message.channel.send('0');
+    await resetCountMsg.react('✔');
+    return;
+  }
+
+  await message.react('✔');
+}
---
+++ src/events/messageUpdate/saveCount.ts
@@ -0,0 +1,28 @@
+import { Client, Message, PartialMessage } from 'discord.js';
+import config from '#config' assert { type: 'json' };
+
+export default async function (
+  _: Client,
+  oldMessage: Message<boolean> | PartialMessage,
+  newMessage: Message<boolean> | PartialMessage
+) {
+  if (newMessage.author?.bot) return;
+  if (!newMessage.inGuild()) return;
+
+  const { countChannelID } = config;
+  if (!countChannelID) return;
+  if (newMessage.channelId !== countChannelID) return;
+
+  const countChannel = newMessage.channel;
+
+  if (countChannel.lastMessage?.author.id !== newMessage.author.id) return;
+
+  await newMessage.delete();
+
+  const oldMsgCount = oldMessage.content!;
+
+  await countChannel.sendTyping();
+
+  const currentCount = await countChannel.send(oldMsgCount);
+  await currentCount.react('✔');
+}
