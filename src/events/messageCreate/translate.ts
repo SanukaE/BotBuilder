739d14ff3afe0a5f31896340681bf9847f0f596f
ItzSanuka <ItzSanuka@outlook.com>
2025-02-03 17:37:27 +0530

    Add auto message translation feature; enable translation via config.json and implement translation logic

---
+++ src/events/messageCreate/translate.ts
@@ -0,0 +1,34 @@
+import { Client, Message, OmitPartialGroupDMChannel } from 'discord.js';
+import config from '#config' assert { type: 'json' };
+import { translate } from 'bing-translate-api';
+
+export default async function (
+  client: Client,
+  message: OmitPartialGroupDMChannel<Message<boolean>>
+) {
+  if (!message.deletable) return;
+
+  const { enableAutoMessageTranslation, translationLanguage } = config;
+  if (!enableAutoMessageTranslation) return;
+
+  if (message.author.bot) return;
+
+  const translationData = await translate(
+    message.content,
+    undefined,
+    translationLanguage,
+    true
+  ).catch((err) => console.log(`[Error] Failed to translate: ${err}`));
+
+  if (!translationData || translationData.language.from === translationLanguage)
+    return;
+
+  await message.channel.sendTyping();
+
+  await message.reply({
+    content: `> Message Translation\n\n${
+      translationData.correctedText || translationData.translation
+    }`,
+    allowedMentions: { repliedUser: false },
+  });
+}
