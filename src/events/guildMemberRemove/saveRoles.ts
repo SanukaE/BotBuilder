10e03726e600c2d4a327ec12c6976424a171303d
SanukaE <ItzSanuka@outlook.com>
2025-04-29 12:35:57 +1000

    Enhance role handling by adding checks for existing roles in database and filtering out guild ID in saveRoles function

--- src/events/guildMemberRemove/saveRoles.ts
+++ src/events/guildMemberRemove/saveRoles.ts
@@ -10,9 +10,9 @@ export default async function (
   if (!enableStickyRoles) return;
 
   const roleIDs = member.roles.cache
-    .filter((role) => role.id !== memberRoleID && role.id !== botRoleID)
+    .filter((role) => role.id !== memberRoleID && role.id !== botRoleID && role.id !== member.guild.id)
     .map((role) => role.id);
-
+  
   await MySQL.query(
     'INSERT INTO user_roles (userID, roles) VALUES (?, ?) ON DUPLICATE KEY UPDATE roles = ?',
     [member.id, JSON.stringify(roleIDs), JSON.stringify(roleIDs)]
