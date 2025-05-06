9c49d2ce27c0b22a88298e39848d0e2e84149ca4
SanukaE <ItzSanuka@outlook.com>
2025-05-06 12:48:12 +1000

    Refactor database setup by removing redundant database creation and usage queries; ensure MYSQL_DATABASE is utilized in MySQL connection.

--- src/libs/MySQL.ts
+++ src/libs/MySQL.ts
@@ -7,6 +7,7 @@ const dbPool = mysql
     port: parseInt(process.env.MYSQL_PORT!),
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
   })
   .promise();
 
