09e16ee7ff01f3579356145f83ea3d8b90095274
SanukaE <ItzSanuka@outlook.com>
2025-07-23 18:54:37 +1000

    fix: Add missing newline at end of tickets.sql file

--- public/sql/tickets.sql
+++ public/sql/tickets.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS tickets (
     category VARCHAR(255) NOT NULL,
     channelID VARCHAR(255) NOT NULL,
-    ownerID VARCHAR(255) NOT NULL,
+    ownerID VARCHAR(255) NOT NULL
 );
