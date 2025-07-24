9f48815f0a939bc0bf85a2c6a05b0ff7c64f6ca9
SanukaE <ItzSanuka@outlook.com>
2025-07-25 00:02:32 +1000

    Fix an issue with syntax

--- public/sql/tickets.sql
+++ public/sql/tickets.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS tickets (
     category VARCHAR(255) NOT NULL,
     channelID VARCHAR(255) NOT NULL,
     ownerID VARCHAR(255) NOT NULL,
-    claimedBy VARCHAR(255)
+    claimedBy VARCHAR(255),
     formData JSON
 );
